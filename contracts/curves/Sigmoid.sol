fda2c2a76080c8cd7407e459595a567e5937eb17
Zokunei <zokunei@icloud.com>
2021-12-14 20:31:48 -0500

    sigmoid

--- contracts/curves/Sigmoid.sol
+++ contracts/curves/Sigmoid.sol
@@ -2,6 +2,25 @@ pragma solidity ^0.8.0;
 
 contract Sigmoid {
 
+    function curve(uint maturity) external pure returns (uint) {
+        uint denom = sqrt(uint((int(maturity) - 2e7) ** 2) + 1e14);
+        return uint(
+            50 * (int(maturity) - 2e7 + int(denom))
+            / int(denom)
+        );
+    }
 
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
 
 }
