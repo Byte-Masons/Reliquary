5deda0069671a74a88b1ace567a04db04da76d6e
Zokunei <zokunei@icloud.com>
2021-12-16 00:50:43 -0500

    added documentation, tweaked constants

--- contracts/curves/Sigmoid.sol
+++ contracts/curves/Sigmoid.sol
@@ -1,17 +1,19 @@
+// EXPERIMENTAL - NO NEED TO AUDIT THIS FILE
+
 pragma solidity ^0.8.0;
 
 contract Sigmoid {
 
     int constant HORIZONTAL_STRETCH = 1e7;
-    int constant HORIZONTAL_SHIFT = 2e7;
+    int constant HORIZONTAL_SHIFT = 25e6;
     int constant VERTICAL_STRETCH = 50;
-    int constant VERTICAL_SHIFT = 0;
+    int constant VERTICAL_SHIFT = 0; // care must be taken not to allow negative y-values
 
     function curve(uint maturity) external pure returns (uint) {
         int denom = sqrt((int(maturity) - HORIZONTAL_SHIFT) ** 2 + HORIZONTAL_STRETCH ** 2);
 
         return uint(
-            VERTICAL_STRETCH * ((int(maturity) - HORIZONTAL_SHIFT) + denom)
+            VERTICAL_STRETCH * ((int(maturity) - HORIZONTAL_SHIFT) + denom) // denom is added to place lower bound at 0
             / denom + VERTICAL_SHIFT
         );
     }
