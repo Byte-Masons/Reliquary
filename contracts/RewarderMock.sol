5bfba22be2fce0469b590fdfc75a629624995881
tess3rac7 <tess3rac7@protonmail.com>
2022-01-13 18:30:16 -0500

    Remove boring files.

--- contracts/RewarderMock.sol
+++ contracts/RewarderMock.sol
@@ -4,10 +4,8 @@ pragma solidity 0.8.9;
 
 import "./interfaces/IRewarder.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./boring-solidity/libraries/BoringMath.sol";
 
 contract RewarderMock is IRewarder {
-    using BoringMath for uint256;
     using SafeERC20 for IERC20;
     uint256 private immutable rewardMultiplier;
     IERC20 private immutable rewardToken;
