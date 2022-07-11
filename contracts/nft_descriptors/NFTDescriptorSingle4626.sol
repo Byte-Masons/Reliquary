7e6bb701bd08e8555363572886bd9841e86e393e
Zokunei <zokunei@bytemasons.com>
2022-07-11 17:53:15 -0400

    add NFTDescriptor for single asset ERC4626 vaults

---
+++ contracts/nft_descriptors/NFTDescriptorSingle4626.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./NFTDescriptor.sol";
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+contract NFTDescriptorSingle4626 is NFTDescriptor {
+    constructor(IReliquary _reliquary) NFTDescriptor(_reliquary) {}
+
+    function generateTextFromToken(
+        address underlying,
+        uint amount,
+        string memory amountString
+    ) internal view override returns (string memory text) {
+        IERC4626 vault = IERC4626(underlying);
+        IERC20Values asset = IERC20Values(vault.asset());
+
+        string memory assetAmount = generateDecimalString(vault.convertToAssets(amount), asset.decimals());
+        text = string.concat(
+            '<text x="50%" y="320" class="bit" style="font-size: 8">', asset.symbol(), ':', assetAmount
+        );
+    }
+}
