30f96dfa8314bdcc36f4c01059b8a0cc1cd4a4f5
Zokunei <zokunei@bytemasons.com>
2022-07-19 18:46:19 -0400

    all tests have asserts

--- test/foundry/DepositHelper.t.sol
+++ test/foundry/DepositHelper.t.sol
@@ -7,36 +7,50 @@ import "contracts/helpers/DepositHelper.sol";
 
 contract DepositHelperTest is Test {
     DepositHelper helper;
+    IReliquary reliquary;
+    IERC4626 vault;
     IERC20 weth;
     address constant WETH_WHALE = 0x2400BB4D7221bA530Daee061D5Afe219E9223Eae;
 
     function setUp() public {
         helper = new DepositHelper(0x90fEC9587624dC4437833Ef3C34C218996B8AB98);
-        weth = IERC20(IERC4626(address(helper.reliquary().poolToken(0))).asset());
+        reliquary = helper.reliquary();
+        vault = IERC4626(address(reliquary.poolToken(0)));
+        weth = IERC20(vault.asset());
 
         vm.startPrank(address(WETH_WHALE), address(WETH_WHALE));
         weth.approve(address(helper), type(uint).max);
+        helper.reliquary().setApprovalForAll(address(helper), true);
     }
 
     function testCreateNew() public {
-        helper.deposit(0, 1e18, 0);
+        helper.deposit(0, 1 ether, 0);
+
+        assertTrue(reliquary.balanceOf(WETH_WHALE) == 1, "no Relic given");
+        assertTrue(
+            reliquary.getPositionForId(_getRelicId()).amount == vault.convertToShares(1 ether),
+            "deposited amount not expected amount"
+        );
     }
 
     function testDepositExisting() public {
-        helper.deposit(0, 1e18, 0);
-        helper.reliquary().setApprovalForAll(address(helper), true);
+        helper.deposit(0, 1 ether, 0);
 
-        helper.deposit(0, 5e17, helper.reliquary().tokenOfOwnerByIndex(WETH_WHALE, 0));
+        helper.deposit(0, 0.5 ether, _getRelicId());
+        uint relicAmount = reliquary.getPositionForId(_getRelicId()).amount;
+        uint expectedAmount = vault.convertToShares(1.5 ether);
+        assertTrue(expectedAmount - 100 < relicAmount && relicAmount < expectedAmount + 100);
     }
 
     function testWithdraw() public {
-        helper.deposit(0, 1e18, 0);
-        helper.reliquary().setApprovalForAll(address(helper), true);
+        helper.deposit(0, 1 ether, 0);
 
         uint initialBalance = weth.balanceOf(WETH_WHALE);
-        helper.withdraw(0, 1e18, helper.reliquary().tokenOfOwnerByIndex(WETH_WHALE, 0));
-        uint diff = weth.balanceOf(WETH_WHALE) - initialBalance;
+        helper.withdraw(0, 1 ether, _getRelicId());
+        assertTrue(weth.balanceOf(WETH_WHALE) == initialBalance);
+    }
 
-        assertTrue(diff == 0);
+    function _getRelicId() private view returns (uint relicId) {
+        relicId = reliquary.tokenOfOwnerByIndex(WETH_WHALE, 0);
     }
 }
