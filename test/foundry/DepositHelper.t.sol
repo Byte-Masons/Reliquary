2f4b35ebb42a72648a934568490eb759c4d55cd4
Zokunei <zokunei@bytemasons.com>
2022-08-15 17:25:04 -0400

    do not rely on forked Reliquary in DepositHelper tests

--- test/foundry/DepositHelper.t.sol
+++ test/foundry/DepositHelper.t.sol
@@ -4,19 +4,33 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "contracts/helpers/DepositHelper.sol";
+import "contracts/Reliquary.sol";
+import "contracts/nft_descriptors/NFTDescriptorSingle4626.sol";
+import "contracts/emission_curves/Constant.sol";
 
 contract DepositHelperTest is Test {
     DepositHelper helper;
-    IReliquary reliquary;
+    Reliquary reliquary;
     IERC4626 vault;
     IERC20 weth;
     address constant WETH_WHALE = 0x2400BB4D7221bA530Daee061D5Afe219E9223Eae;
 
+    uint[] wethCurve = [0, 1 days, 7 days, 14 days, 30 days, 90 days, 180 days, 365 days];
+    uint[] wethLevels = [100, 120, 150, 200, 300, 400, 500, 750];
+
     function setUp() public {
         vm.createSelectFork("fantom", 43052549);
-        helper = new DepositHelper(0x90fEC9587624dC4437833Ef3C34C218996B8AB98);
-        reliquary = helper.reliquary();
-        vault = IERC4626(address(reliquary.poolToken(0)));
+
+        reliquary = new Reliquary(
+            IERC20(0x21Ada0D2aC28C3A5Fa3cD2eE30882dA8812279B6),
+            IEmissionCurve(address(new Constant()))
+        );
+        vault = IERC4626(0x58C60B6dF933Ff5615890dDdDCdD280bad53f1C1);
+        INFTDescriptor nftDescriptor = INFTDescriptor(new NFTDescriptorSingle4626(IReliquary(reliquary)));
+        reliquary.grantRole(keccak256(bytes("OPERATOR")), address(this));
+        reliquary.addPool(1000, vault, IRewarder(address(0)), wethCurve, wethLevels, "ETH Crypt", nftDescriptor);
+
+        helper = new DepositHelper(address(reliquary));
         weth = IERC20(vault.asset());
 
         vm.startPrank(WETH_WHALE);
