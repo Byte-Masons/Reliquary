b4f5adb0a4bebf09923f6d264a621a4e4be7cb7a
Zokunei <zokunei@bytemasons.com>
2022-10-28 16:29:41 -0400

    refactor UseRandom

--- contracts/gamification/UseRandom.sol
+++ contracts/gamification/UseRandom.sol
@@ -2,25 +2,26 @@ pragma solidity ^0.8.17;
 
 import "../libraries/SlothVDF.sol";
 
-contract UseRandom {
+abstract contract UseRandom {
     // large prime
-    uint256 public prime = 432211379112113246928842014508850435796007;
+    uint public constant PRIME = 432211379112113246928842014508850435796007;
     // adjust for block finality
-    uint256 public iterations = 1000;
+    uint public constant ITERATIONS = 1000;
     // increment nonce to increase entropy
-    uint256 private nonce;
+    uint private nonce;
     // address -> vdf seed
-    mapping(address => uint256) public seeds;
+    mapping(address => uint) public seeds;
 
     function createSeed() external {
-        // commit funds/tokens/etc here
+        // commit funds/tokens/etc here?
+
         // create a pseudo random seed as the input
-        seeds[msg.sender] = uint256(keccak256(abi.encodePacked(msg.sender, nonce++, block.timestamp, blockhash(block.number - 1))));
+        seeds[msg.sender] = uint(keccak256(abi.encodePacked(msg.sender, nonce++, block.timestamp, blockhash(block.number - 1))));
     }
 
-    function _prove(uint256 proof) internal view {
+    function _prove(uint proof) internal view {
         // see if the proof is valid for the seed associated with the address
-        require(SlothVDF.verify(proof, seeds[msg.sender], prime, iterations), 'Invalid proof');
+        require(SlothVDF.verify(proof, seeds[msg.sender], PRIME, ITERATIONS), 'Invalid proof');
 
         // use the proof as a provable random number
     }
