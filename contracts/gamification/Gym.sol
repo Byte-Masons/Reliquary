f10d07bdf0eada3b64d326b704d2089dcec54a97
Zokunei <zokunei@bytemasons.com>
2022-10-28 16:37:18 -0400

    remove item minting from Gym

--- contracts/gamification/Gym.sol
+++ contracts/gamification/Gym.sol
@@ -10,14 +10,6 @@ interface IAvatar {
     function getBonus(uint id) external returns (uint);
 }
 
-interface IItemSet is IERC721Enumerable {
-    function mint(address to) external;
-    // if fully minted return false, if turned off return false
-    function isValidSet() external returns (bool);
-    // return true if not paused and hasn't hit max supply
-    function isMintable() external returns (bool);
-}
-
 contract Gym is UseRandom, Ownable {
     struct Avatar {
         IAvatar collection;
@@ -28,8 +20,6 @@ contract Gym is UseRandom, Ownable {
 
     IReliquary public reliquary;
     mapping(address => Avatar) public avatars;
-    IItemSet public itemSet;
-    bool public itemToggle = false;
 
     constructor(IReliquary _reliquary) {
         reliquary = _reliquary;
@@ -54,30 +44,12 @@ contract Gym is UseRandom, Ownable {
         );
 
         uint n = rand % 1 days;
-        if (n / 100 == 0 && _canMintItem()) {
-            itemSet.mint(msg.sender);
-        } else {
-            Avatar storage ava = avatars[msg.sender];
-            if (ava.id != 0) {
-                n = n * ava.collection.getBonus(ava.id) / BASIS_POINTS;
-            }
-            //smarter rounding so it's an equal chance of hitting 24
-            n = n / 1 hours * 1 hours;
-            reliquary.modifyMaturity(relicId, n);
+        Avatar storage ava = avatars[msg.sender];
+        if (ava.id != 0) {
+            n = n * ava.collection.getBonus(ava.id) / BASIS_POINTS;
         }
-    }
-
-    function _canMintItem() internal returns (bool) {
-        return itemSet.isMintable() && itemToggle;
-    }
-
-    function toggleItems(bool status) external onlyOwner {
-        require(address(itemSet) != address(0), "current itemset is invalid");
-        itemToggle = status;
-    }
-
-    function updateItemSet(IItemSet newSet) external onlyOwner {
-        require(itemSet.isValidSet(), "current itemset is invalid");
-        itemSet = newSet;
+        //smarter rounding so it's an equal chance of hitting 24
+        n = n / 1 hours * 1 hours;
+        reliquary.modifyMaturity(relicId, n);
     }
 }
