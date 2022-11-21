7eb4a67f1bee2d9e73eec5e4c8f494426acb3005
Zokunei <zokunei@bytemasons.com>
2022-11-21 18:07:44 -0500

    update Gym documentation

--- contracts/gamification/Gym.sol
+++ contracts/gamification/Gym.sol
@@ -3,27 +3,18 @@ pragma solidity ^0.8.17;
 import "./UseRandom.sol";
 import "../interfaces/IReliquary.sol";
 
-/*interface IAvatar {
-    // returns basis points
-    function getBonus(uint id) external returns (uint);
-}*/
-
 contract Gym is UseRandom {
-    /*struct Avatar {
-        IAvatar collection;
-        uint96 id;
-    }*/
-
-    //uint private constant BASIS_POINTS = 10_000;
 
     IReliquary public reliquary;
-    //mapping(address => Avatar) public avatars;
     mapping(uint => uint) seeds;
 
     constructor(IReliquary _reliquary) {
         reliquary = _reliquary;
     }
 
+    /// @notice Assign same random seed to each Relic. May be called no more than once per day per Relic
+    /// @param relicIds Array of relicIds belonging to msg.sender (may or may not be all of them)
+    /// @return seed The seed used to generate a provably random number via VDF
     function createSeed(uint[] calldata relicIds) external returns (uint seed) {
         seed = _createSeed();
 
@@ -42,16 +33,16 @@ contract Gym is UseRandom {
         }
     }
 
+    /// @notice Apply the maturity bonus derived from the seed assigned by createSeed function
+    /// @param relicIds Array of relicIds which all have the same seed
+    /// @param proof The provably random number derived from the VDF.
+    ///        Used as number of seconds by which to reduce position's entry time.
     function train(uint[] calldata relicIds, uint proof) external {
         uint seed = seeds[relicIds[0]];
         require(seed != 0, "no seed");
         _prove(proof, seed);
 
         uint n = proof % 1 days;
-        /*Avatar memory avatar = avatars[msg.sender];
-        if (avatar.id != 0) {
-            n = n * avatar.collection.getBonus(avatar.id) / BASIS_POINTS;
-        }*/
 
         _train(relicIds[0], n);
         for (uint i = 1; i < relicIds.length;) {
@@ -61,6 +52,9 @@ contract Gym is UseRandom {
         }
     }
 
+    /// @notice Internal function which allows to apply effects to first relicId without redundant seed mismatch check
+    /// @param relicId The NFT ID of the Relic being trained
+    /// @param rand The provably random number derived from the VDF
     function _train(uint relicId, uint rand) internal {
         delete seeds[relicId];
         reliquary.modifyMaturity(relicId, rand);
