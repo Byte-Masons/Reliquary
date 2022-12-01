6d1a379cf13e2dba02ea17b0a2b824cd3bf4b9b1
Zokunei <zokunei@bytemasons.com>
2022-12-01 14:51:04 -0500

    update forge-std to master

--- contracts/interfaces/IReliquary.sol
+++ contracts/interfaces/IReliquary.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.15;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
@@ -8,14 +7,15 @@ import "./IEmissionCurve.sol";
 import "./INFTDescriptor.sol";
 import "./IRewarder.sol";
 
-/*
- + @notice Info for each Reliquary position.
- + `amount` LP token amount the position owner has provided
- + `rewardDebt` Amount of reward token accumalated before the position's entry or last harvest
- + `rewardCredit` Amount of reward token owed to the user on next harvest
- + `entry` Used to determine the maturity of the position
- + `poolId` ID of the pool to which this position belongs
- + `level` Index of this position's level within the pool's array of levels*/
+/**
+ * @notice Info for each Reliquary position.
+ * `amount` LP token amount the position owner has provided.
+ * `rewardDebt` Amount of reward token accumalated before the position's entry or last harvest.
+ * `rewardCredit` Amount of reward token owed to the user on next harvest.
+ * `entry` Used to determine the maturity of the position.
+ * `poolId` ID of the pool to which this position belongs.
+ * `level` Index of this position's level within the pool's array of levels.
+ */
 struct PositionInfo {
     uint amount;
     uint rewardDebt;
@@ -25,12 +25,13 @@ struct PositionInfo {
     uint level;
 }
 
-/*
- + @notice Info of each Reliquary pool
- + `accRewardPerShare` Accumulated reward tokens per share of pool (1 / 1e12)
- + `lastRewardTime` Last timestamp the accumulated reward was updated
- + `allocPoint` Pool's individual allocation - ratio of the total allocation
- + `name` Name of pool to be displayed in NFT image*/
+/**
+ * @notice Info of each Reliquary pool.
+ * `accRewardPerShare` Accumulated reward tokens per share of pool (1 / 1e12).
+ * `lastRewardTime` Last timestamp the accumulated reward was updated.
+ * `allocPoint` Pool's individual allocation - ratio of the total allocation.
+ * `name` Name of pool to be displayed in NFT image.
+ */
 struct PoolInfo {
     uint accRewardPerShare;
     uint lastRewardTime;
@@ -38,22 +39,24 @@ struct PoolInfo {
     string name;
 }
 
-/*
- + @notice Level that determines how maturity is rewarded
- + `requiredMaturity` The minimum maturity (in seconds) required to reach this Level
- + `allocPoint` Level's individual allocation - ratio of the total allocation
- + `balance` Total number of tokens deposited in positions at this Level*/
+/**
+ * @notice Level that determines how maturity is rewarded.
+ * `requiredMaturity` The minimum maturity (in seconds) required to reach this Level.
+ * `allocPoint` Level's individual allocation - ratio of the total allocation.
+ * `balance` Total number of tokens deposited in positions at this Level.
+ */
 struct LevelInfo {
     uint[] requiredMaturity;
     uint[] allocPoint;
     uint[] balance;
 }
 
-/*
- + @notice Object representing pending rewards and related data for a position.
- + `relicId` The NFT ID of the given position.
- + `poolId` ID of the pool to which this position belongs.
- + `pendingReward` pending reward amount for a given position.*/
+/**
+ * @notice Object representing pending rewards and related data for a position.
+ * `relicId` The NFT ID of the given position.
+ * `poolId` ID of the pool to which this position belongs.
+ * `pendingReward` pending reward amount for a given position.
+ */
 struct PendingReward {
     uint relicId;
     uint poolId;
@@ -102,7 +105,6 @@ interface IReliquary is IERC721Enumerable {
     function merge(uint fromId, uint toId) external;
 
     // State
-
     function rewardToken() external view returns (IERC20);
     function nftDescriptor(uint) external view returns (INFTDescriptor);
     function emissionCurve() external view returns (IEmissionCurve);
@@ -110,7 +112,6 @@ interface IReliquary is IERC721Enumerable {
     function getLevelInfo(uint) external view returns (LevelInfo memory);
     function poolToken(uint) external view returns (IERC20);
     function rewarder(uint) external view returns (IRewarder);
-
     function getPositionForId(uint) external view returns (PositionInfo memory);
     function totalAllocPoint() external view returns (uint);
     function poolLength() external view returns (uint);
