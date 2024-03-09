9f140b16e6da8d25e81d42255ad7243eb4c5b2ab
Zokunei <zokunei@bytemasons.com>
2024-03-08 19:17:29 -0500

    update deploy script

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -1,144 +1,190 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-// import "forge-std/Script.sol";
-// import {Reliquary} from "contracts/Reliquary.sol";
-// import {DepositHelperERC4626} from "contracts/helpers/DepositHelperERC4626.sol";
-// import {NFTDescriptor, NFTDescriptorPair} from "contracts/nft_descriptors/NFTDescriptorPair.sol";
-// import {NFTDescriptorSingle4626} from "contracts/nft_descriptors/NFTDescriptorSingle4626.sol";
-// import {ParentRewarder} from "contracts/rewarders/ParentRewarder.sol";
+import "forge-std/Script.sol";
+import {Reliquary} from "contracts/Reliquary.sol";
+import {ICurves, LinearCurve} from "contracts/curves/LinearCurve.sol";
+import {LinearPlateauCurve} from "contracts/curves/LinearPlateauCurve.sol";
+import {DepositHelperERC4626} from "contracts/helpers/DepositHelperERC4626.sol";
+import {NFTDescriptor, NFTDescriptorPair} from "contracts/nft_descriptors/NFTDescriptorPair.sol";
+import {NFTDescriptorSingle4626} from "contracts/nft_descriptors/NFTDescriptorSingle4626.sol";
+import {ParentRollingRewarder} from "contracts/rewarders/ParentRollingRewarder.sol";
 
-// contract Deploy is Script {
-//     using stdJson for string;
+contract Deploy is Script {
+    using stdJson for string;
 
-//     struct Pool {
-//         uint allocPoint;
-//         bool allowPartialWithdrawals;
-//         uint[] levelMultipliers;
-//         string name;
-//         address poolToken;
-//         uint[] requiredMaturities;
-//         int rewarderIndex;
-//         string tokenType;
-//     }
+    struct Pool {
+        uint allocPoint;
+        bool allowPartialWithdrawals;
+        uint curveIndex;
+        string curveType;
+        string name;
+        address poolToken;
+        string tokenType;
+    }
 
-//     struct Rewarder {
-//         int parentIndex;
-//         uint rewardMultiplier;
-//         address rewarderToken;
-//     }
+    struct ParentRewarderParams {
+        uint poolId;
+    }
 
-//     bytes32 constant OPERATOR = keccak256("OPERATOR");
-//     bytes32 constant EMISSION_RATE = keccak256("EMISSION_RATE");
-//     bytes32 constant CHILD_SETTER = keccak256("CHILD_SETTER");
+    struct ChildRewarderParams {
+        uint parentIndex;
+        address rewarderToken;
+    }
 
-//     string config;
-//     address multisig;
-//     Reliquary reliquary;
-//     address[] rewarderAddresses;
-//     ParentRewarder[] parentRewarders;
-//     address nftDescriptorNormal;
-//     address nftDescriptor4626;
-//     address nftDescriptorPair;
-//     address depositHelper4626;
+    struct LinearCurveParams {
+        uint minMultiplier;
+        uint slope;
+    }
 
-//     function run() external {
-//         config = vm.readFile("scripts/deploy_conf.json");
-//         string memory name = config.readString(".name");
-//         string memory symbol = config.readString(".symbol");
-//         multisig = config.readAddress(".multisig");
-//         address rewardToken = config.readAddress(".rewardToken");
-//         uint emissionRate = config.readUint(".emissionRate");
-//         Pool[] memory pools = abi.decode(config.parseRaw(".pools"), (Pool[]));
+    struct LinearPlateauCurveParams {
+        uint minMultiplier;
+        uint plateauLevel;
+        uint slope;
+    }
 
-//         vm.startBroadcast();
+    bytes32 constant OPERATOR = keccak256("OPERATOR");
+    bytes32 constant EMISSION_RATE = keccak256("EMISSION_RATE");
 
-//         reliquary = new Reliquary(rewardToken, emissionRate, name, symbol);
+    string config;
+    address multisig;
+    Reliquary reliquary;
+    uint poolCount;
+    address rewardToken;
+    mapping(uint => ParentRollingRewarder) parentForPoolId;
+    LinearCurve[] linearCurves;
+    LinearPlateauCurve[] linearPlateauCurves;
+    address nftDescriptorNormal;
+    address nftDescriptor4626;
+    address nftDescriptorPair;
+    address depositHelper4626;
 
-//         _deployRewarders();
+    function run() external {
+        config = vm.readFile("scripts/deploy_conf.json");
+        string memory name = config.readString(".name");
+        string memory symbol = config.readString(".symbol");
+        multisig = config.readAddress(".multisig");
+        rewardToken = config.readAddress(".rewardToken");
+        uint emissionRate = config.readUint(".emissionRate");
+        Pool[] memory pools = abi.decode(config.parseRaw(".pools"), (Pool[]));
+        poolCount = pools.length;
 
-//         reliquary.grantRole(OPERATOR, tx.origin);
-//         for (uint i = 0; i < pools.length; ++i) {
-//             Pool memory pool = pools[i];
+        vm.startBroadcast();
 
-//             address nftDescriptor = _deployHelpers(pool.tokenType);
+        _deployCurves();
 
-//             reliquary.addPool(
-//                 pool.allocPoint,
-//                 pool.poolToken,
-//                 pool.rewarderIndex < 0 ? address(0) : rewarderAddresses[uint(pool.rewarderIndex)],
-//                 pool.requiredMaturities,
-//                 pool.levelMultipliers,
-//                 pool.name,
-//                 nftDescriptor,
-//                 pool.allowPartialWithdrawals
-//             );
-//         }
+        reliquary = new Reliquary(rewardToken, emissionRate, name, symbol);
 
-//         if (multisig != address(0)) {
-//             _renounceRoles();
-//         }
+        _deployRewarders();
 
-//         vm.stopBroadcast();
-//     }
+        reliquary.grantRole(OPERATOR, tx.origin);
+        for (uint i = 0; i < pools.length; ++i) {
+            Pool memory pool = pools[i];
 
-//     function _deployRewarders() internal {
-//         Rewarder[] memory rewarders = abi.decode(config.parseRaw(".rewarders"), (Rewarder[]));
-//         for (uint i; i < rewarders.length; ++i) {
-//             Rewarder memory rewarder = rewarders[i];
+            ICurves curve;
+            bytes32 curveTypeHash = keccak256(bytes(pool.curveType));
+            if (curveTypeHash == keccak256("linearCurve")) {
+                curve = linearCurves[pool.curveIndex];
+            } else if (curveTypeHash == keccak256("linearPlateauCurve")) {
+                curve = linearPlateauCurves[pool.curveIndex];
+            } else {
+                revert(string.concat("invalid curve type ", pool.curveType));
+            }
 
-//             if (rewarder.parentIndex < 0) {
-//                 ParentRewarder newParent =
-//                     new ParentRewarder(rewarder.rewardMultiplier, rewarder.rewarderToken, address(reliquary));
-//                 newParent.grantRole(CHILD_SETTER, tx.origin);
+            address nftDescriptor = _deployHelpers(pool.tokenType);
 
-//                 parentRewarders.push(newParent);
-//                 rewarderAddresses.push(address(newParent));
-//             } else {
-//                 rewarderAddresses.push(
-//                     ParentRewarder(rewarderAddresses[uint(rewarder.parentIndex)]).createChild(
-//                         rewarder.rewarderToken, rewarder.rewardMultiplier, multisig != address(0) ? multisig : tx.origin
-//                     )
-//                 );
-//             }
-//         }
-//     }
+            reliquary.addPool(
+                pool.allocPoint,
+                pool.poolToken,
+                address(parentForPoolId[i]),
+                curve,
+                pool.name,
+                nftDescriptor,
+                pool.allowPartialWithdrawals
+            );
+        }
 
-//     function _deployHelpers(string memory poolTokenType) internal returns (address nftDescriptor) {
-//         bytes32 typeHash = keccak256(bytes(poolTokenType));
-//         if (typeHash == keccak256("normal")) {
-//             if (nftDescriptorNormal == address(0)) {
-//                 nftDescriptorNormal = address(new NFTDescriptor(address(reliquary)));
-//             }
-//             nftDescriptor = nftDescriptorNormal;
-//         } else if (typeHash == keccak256("4626")) {
-//             if (nftDescriptor4626 == address(0)) {
-//                 nftDescriptor4626 = address(new NFTDescriptorSingle4626(address(reliquary)));
-//             }
-//             nftDescriptor = nftDescriptor4626;
-//             if (depositHelper4626 == address(0)) {
-//                 depositHelper4626 = address(new DepositHelperERC4626(reliquary, config.readAddress(".weth")));
-//             }
-//         } else if (typeHash == keccak256("pair")) {
-//             if (nftDescriptorPair == address(0)) {
-//                 nftDescriptorPair = address(new NFTDescriptorPair(address(reliquary)));
-//             }
-//             nftDescriptor = nftDescriptorPair;
-//         } else {
-//             revert(string.concat("invalid token type ", poolTokenType));
-//         }
-//     }
+        if (multisig != address(0)) {
+            _renounceRoles();
+        }
 
-//     function _renounceRoles() internal {
-//         bytes32 defaultAdminRole = reliquary.DEFAULT_ADMIN_ROLE();
-//         reliquary.grantRole(defaultAdminRole, multisig);
-//         reliquary.grantRole(OPERATOR, multisig);
-//         reliquary.grantRole(EMISSION_RATE, multisig);
-//         reliquary.renounceRole(OPERATOR, tx.origin);
-//         reliquary.renounceRole(defaultAdminRole, tx.origin);
-//         for (uint i; i < parentRewarders.length; ++i) {
-//             parentRewarders[i].renounceRole(defaultAdminRole, tx.origin);
-//             parentRewarders[i].renounceRole(CHILD_SETTER, tx.origin);
-//         }
-//     }
-// }
+        vm.stopBroadcast();
+    }
+
+    function _deployRewarders() internal {
+        ParentRewarderParams[] memory parentParams = abi.decode(config.parseRaw(".parentRewarders"), (ParentRewarderParams[]));
+        ParentRollingRewarder[] memory parentRewarders = new ParentRollingRewarder[](parentParams.length);
+        for (uint i; i < parentParams.length; ++i) {
+            ParentRewarderParams memory params = parentParams[i];
+
+            ParentRollingRewarder newParent = new ParentRollingRewarder();
+
+            parentRewarders[i] = newParent;
+            parentForPoolId[params.poolId] = newParent;
+        }
+
+        ChildRewarderParams[] memory children = abi.decode(config.parseRaw(".childRewarders"), (ChildRewarderParams[]));
+        for (uint i; i < children.length; ++i) {
+            ChildRewarderParams memory child = children[i];
+            ParentRollingRewarder parent = ParentRollingRewarder(parentRewarders[child.parentIndex]);
+            parent.createChild(child.rewarderToken);
+        }
+    }
+
+    function _deployCurves() internal {
+        LinearCurveParams[] memory linearCurveParams =
+            abi.decode(config.parseRaw(".linearCurves"), (LinearCurveParams[]));
+        for (uint i; i < linearCurveParams.length; ++i) {
+            LinearCurveParams memory params = linearCurveParams[i];
+            linearCurves.push(new LinearCurve(params.slope, params.minMultiplier));
+        }
+
+        LinearPlateauCurveParams[] memory linearPlateauCurveParams =
+            abi.decode(config.parseRaw(".linearPlateauCurves"), (LinearPlateauCurveParams[]));
+        for (uint i; i < linearPlateauCurveParams.length; ++i) {
+            LinearPlateauCurveParams memory params = linearPlateauCurveParams[i];
+            linearPlateauCurves.push(new LinearPlateauCurve(params.slope, params.minMultiplier, params.plateauLevel));
+        }
+    }
+
+    function _deployHelpers(string memory poolTokenType) internal returns (address nftDescriptor) {
+        bytes32 typeHash = keccak256(bytes(poolTokenType));
+        if (typeHash == keccak256("normal")) {
+            if (nftDescriptorNormal == address(0)) {
+                nftDescriptorNormal = address(new NFTDescriptor(address(reliquary)));
+            }
+            nftDescriptor = nftDescriptorNormal;
+        } else if (typeHash == keccak256("4626")) {
+            if (nftDescriptor4626 == address(0)) {
+                nftDescriptor4626 = address(new NFTDescriptorSingle4626(address(reliquary)));
+            }
+            nftDescriptor = nftDescriptor4626;
+            if (depositHelper4626 == address(0)) {
+                depositHelper4626 = address(new DepositHelperERC4626(reliquary, config.readAddress(".weth")));
+            }
+        } else if (typeHash == keccak256("pair")) {
+            if (nftDescriptorPair == address(0)) {
+                nftDescriptorPair = address(new NFTDescriptorPair(address(reliquary)));
+            }
+            nftDescriptor = nftDescriptorPair;
+        } else {
+            revert(string.concat("invalid token type ", poolTokenType));
+        }
+    }
+
+    function _renounceRoles() internal {
+        bytes32 defaultAdminRole = reliquary.DEFAULT_ADMIN_ROLE();
+        reliquary.grantRole(defaultAdminRole, multisig);
+        reliquary.grantRole(OPERATOR, multisig);
+        reliquary.grantRole(EMISSION_RATE, multisig);
+        reliquary.renounceRole(OPERATOR, tx.origin);
+        reliquary.renounceRole(defaultAdminRole, tx.origin);
+        if (multisig != address(0)) {
+            for (uint i; i < poolCount; ++i) {
+                if (address(parentForPoolId[i]) != address(0)) {
+                    parentForPoolId[i].transferOwnership(multisig);
+                }
+            }
+        }
+    }
+}
